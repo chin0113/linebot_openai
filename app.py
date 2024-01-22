2850fa6a41f2641637dbee33ba4312c9aa0d7ff0
chin0113 <97650035+chin0113@users.noreply.github.com>
2024-01-22 23:14:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,46 @@
-import requests, json
-headers = {'Authorization':'1PlQGmb524SP8EccC6ZKIvX47fzf0u9pRZy0E4oCjx71d5gTBTy2U+JzlcfWMc10r4haBWSJHSv7kIE/cnRCnFM6VNtF3CMmTzVAR7n7xtlyiJs3RuuMXhPq+xOv4f9IJontF4iVL8amDiYMJlUxCAdB04t89/1O/w1cDnyilFU=','Content-Type':'application/json'}
-body = {
-    'replyToken':'ddd4a0b2e26f45278fdda78cd481e6d8',
-    'messages':[{
-            'type': 'text',
-            'text': 'hello'
-        }]
-}
-req = requests.request('POST', 'https://test01-1fbw.onrender.com/callback', headers=headers,data=json.dumps(body).encode('utf-8'))
-print(req.text)
+import json
+import os
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import *
+
+app = Flask(__name__)
+
+@app.route("/callback", methods=['POST'])
+def linebot():
+    body = request.get_data(as_text=True)
+    json_data = json.loads(body)
+    # print(json_data)
+    
+    try:
+        line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
+        handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
+        # openai.api_key = os.getenv('OPENAI_API_KEY')
+        
+        signature = request.headers['X-Line-Signature']
+        handler.handle(body, signature)
+        
+        tp = json_data['events'][0]['message']['type']
+        tk = json_data['events'][0]['replyToken']      # 取得 reply token
+        
+        if tp == 'text':
+            msg = json_data['events'][0]['message']['text']  # 取得 LINE 收到的文字訊息
+            reply = msg
+        # 判斷如果是圖片
+        elif tp == 'image':
+            msgID = json_data['events'][0]['message']['id']  # 取得訊息 id
+            message_content = line_bot_api.get_message_content(msgID)  # 根據訊息 ID 取得訊息內容
+            # 在同樣的資料夾中建立以訊息 ID 為檔名的 .jpg 檔案
+            with open(f'{msgID}.jpg', 'wb') as fd:
+                fd.write(message_content.content)             # 以二進位的方式寫入檔案
+            reply = '圖片儲存完成！'                             # 設定要回傳的訊息
+        else:
+            reply = '你傳的不是文字或圖片呦～'
+        print(reply)
+    except:
+        print(body)                                            # 如果發生錯誤，印出收到的內容
+    return 'OK'   
+      
+if __name__ == "__main__":
+    app.run()

e3ca6228812b28f75afa31cb3102ccd065418457
chin0113 <97650035+chin0113@users.noreply.github.com>
2024-01-22 23:27:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import *
 
+import smtplib
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
 app = Flask(__name__)
 
 @app.route("/callback", methods=['POST'])
@@ -31,9 +36,7 @@ def linebot():
         elif tp == 'image':
             msgID = json_data['events'][0]['message']['id']  # 取得訊息 id
             message_content = line_bot_api.get_message_content(msgID)  # 根據訊息 ID 取得訊息內容
-            # 在同樣的資料夾中建立以訊息 ID 為檔名的 .jpg 檔案
-            with open(f'{msgID}.jpg', 'wb') as fd:
-                fd.write(message_content.content)             # 以二進位的方式寫入檔案
+            sendEmail('LINE 傳圖片來囉！', message_content.content, f'{msgID}.jpg', 'chean0847@msn.com', 'c721204520alu5293')
             reply = '圖片儲存完成！'                             # 設定要回傳的訊息
         else:
             reply = '你傳的不是文字或圖片呦～'
@@ -41,6 +44,21 @@ def linebot():
     except:
         print(body)                                            # 如果發生錯誤，印出收到的內容
     return 'OK'   
-      
+
+def sendEmail(txt, source, fileName, email, pwd):
+    msg = MIMEMultipart()
+    attach_file = MIMEApplication(source, Name=fileName)  # 附加檔案
+    msg.attach(attach_file)
+    msg['Subject'] = txt   # 標題
+    msg['From'] = email    # 給誰 ( 通常是給自己 )
+    msg['To'] = email      # 寄件者
+    smtp = smtplib.SMTP('smtp.gmail.com', 587)
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login(email, pwd) # 使用應用程式密碼登入
+    status = smtp.send_message(msg)
+    print(status)
+    smtp.quit()
+    
 if __name__ == "__main__":
     app.run()

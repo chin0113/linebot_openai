9b3afc5e4c24eecc4a35aca7314a3fe2d6f30aa6
chin0113 <97650035+chin0113@users.noreply.github.com>
2024-01-22 13:34:55 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,95 +1,25 @@
 import json
-
-from flask import Flask, request, abort
-
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import *
-
-#======python的函數庫==========
-import tempfile, os
-import datetime
-import openai
-import time
-import traceback
-#======python的函數庫==========
-
-app = Flask(__name__)
-static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
-# Channel Access Token
-line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
-# Channel Secret
-handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
-# OPENAI API Key初始化設定
-openai.api_key = os.getenv('OPENAI_API_KEY')
-
-
-# 監聽所有來自 /callback 的 Post Request
-@app.route("/callback", methods=['POST'])
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-    return 'OK'
-
-
-# 處理訊息
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    # body = request.get_data(as_text=True)
-    # json_data = json.loads(body)
-    msg = event.message.text
-    try:
-        tp = event.message.type
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(msg))
-    except:
-        print(traceback.format_exc())
-        # line_bot_api.reply_message(event.reply_token, TextSendMessage('你所使用的OPENAI API key額度可能已經超過，請於後台Log內確認錯誤訊息'))
-        
-
-@handler.add(MessageEvent, message=StickerMessage)
-def handle_sticker_message(event):
-    try:
-        body = request.get_data(as_text=True)
-        json_data = json.loads(body)
-        # print(json_data)
-        # stickerId = events.message.stickerId
-        # packageId = events.message.packageId
-        stickerId = json_data['events'][0]['message']['stickerId'] # 取得 stickerId
-        packageId = json_data['events'][0]['message']['packageId'] # 取得 packageId
-        sticker_message = StickerSendMessage(sticker_id=stickerId, package_id=packageId) # 設定要回傳的表情貼圖
-        line_bot_api.reply_message(event.reply_token, sticker_message)
-    except:
-        print(traceback.format_exc())
-        
-        
-@handler.add(PostbackEvent)
-def handle_message(event):
-    print(event.postback.data)
-
-
-@handler.add(MemberJoinedEvent)
-def welcome(event):
-    uid = event.joined.members[0].user_id
-    gid = event.source.group_id
-    profile = line_bot_api.get_group_member_profile(gid, uid)
-    name = profile.display_name
-    message = TextSendMessage(text=f'{name}歡迎加入')
-    line_bot_api.reply_message(event.reply_token, message)
-        
-        
 import os
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, StickerSendMessage
+
+def linebot(request):
+    try:
+        line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
+        handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
+        signature = request.headers['X-Line-Signature']
+        handler.handle(body, signature)
+        
+        tp = json_data['events'][0]['message']['type']
+        tk = json_data['events'][0]['replyToken']      # 取得 reply token
+        
+        if tp == 'sticker':
+            stickerId = json_data['events'][0]['message']['stickerId'] # 取得 stickerId
+            packageId = json_data['events'][0]['message']['packageId'] # 取得 packageId
+            sticker_message = StickerSendMessage(sticker_id=stickerId, package_id=packageId) # 設定要回傳的表情貼圖
+            line_bot_api.reply_message(tk,sticker_message)  # 回傳訊息
+    except:
+        print(json_data)
+    return 'OK'

abc7dd7b0439e0476a19a78a68c7a6d676f4e649
chin0113 <97650035+chin0113@users.noreply.github.com>
2024-01-22 14:02:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent, TextMessage, TextSendMessage, StickerSendMessage
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, StickerSendMessage, ImageSendMessage
 
 app = Flask(__name__)
 
@@ -12,8 +12,6 @@ def linebot():
     body = request.get_data(as_text=True)
     json_data = json.loads(body)
     try:
-        # static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
-        
         line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
         handler = WebhookHandler(os.getenv('CHANNEL_SECRET'))
         # openai.api_key = os.getenv('OPENAI_API_KEY')
@@ -24,6 +22,17 @@ def linebot():
         tp = json_data['events'][0]['message']['type']
         tk = json_data['events'][0]['replyToken']      # 取得 reply token
         
+        if tp == 'text':
+            msg = json_data['events'][0]['message']['text']
+            
+            img_url = reply_img(msg)
+            if img_url:
+            # 如果有圖片網址，回傳圖片
+                img_message = ImageSendMessage(original_content_url=img_url, preview_image_url=img_url)
+                line_bot_api.reply_message(tk,img_message)
+            else:
+                line_bot_api.reply_message(tk,TextSendMessage(msg))
+            
         if tp == 'sticker':
             stickerId = json_data['events'][0]['message']['stickerId'] # 取得 stickerId
             packageId = json_data['events'][0]['message']['packageId'] # 取得 packageId
@@ -32,6 +41,17 @@ def linebot():
     except:
         print(json_data)
     return 'OK'
+
+def reply_img(text):
+    img = {
+        '皮卡丘':'https://upload.wikimedia.org/wikipedia/en/a/a6/Pok%C3%A9mon_Pikachu_art.png',
+        '傑尼龜':'https://upload.wikimedia.org/wikipedia/en/5/59/Pok%C3%A9mon_Squirtle_art.png'
+    }
+    if text in img:
+      return img[text]
+    else:
+      # 如果找不到對應的圖片，回傳 False
+      return False
 
 if __name__ == "__main__":
     app.run()

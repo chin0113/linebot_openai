e8972159f5e50040984889fc3f88655389237a8d
chin0113 <97650035+chin0113@users.noreply.github.com>
2024-01-23 00:54:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -26,17 +26,46 @@ def linebot():
         
         if tp == 'text':
             line_bot_api.push_message('U2574668b48e37ef5423509b4e2355321', TemplateSendMessage(
-                alt_text='ConfirmTemplate',
-                template=ConfirmTemplate(
-                    text='你好嗎？',
-                    actions=[
-                        MessageAction(
-                            label='好喔',
-                            text='好喔'
+                alt_text='CarouselTemplate',
+                template=CarouselTemplate(
+                    columns=[
+                        CarouselColumn(
+                            thumbnail_image_url='https://steam.oxxostudio.tw/download/python/line-template-message-demo.jpg',
+                            title='選單 1',
+                            text='說明文字 1',
+                            actions=[
+                                PostbackAction(
+                                    label='postback',
+                                    data='data1'
+                                ),
+                                MessageAction(
+                                    label='hello',
+                                    text='hello'
+                                ),
+                                URIAction(
+                                    label='oxxo.studio',
+                                    uri='http://oxxo.studio'
+                                )
+                            ]
                         ),
-                        MessageAction(
-                            label='不好喔',
-                            text='不好喔'
+                        CarouselColumn(
+                            thumbnail_image_url='https://steam.oxxostudio.tw/download/python/line-template-message-demo2.jpg',
+                            title='選單 2',
+                            text='說明文字 2',
+                            actions=[
+                                PostbackAction(
+                                    label='postback',
+                                    data='data1'
+                                ),
+                                MessageAction(
+                                    label='hi',
+                                    text='hi'
+                                ),
+                                URIAction(
+                                    label='STEAM 教育學習網',
+                                    uri='https://steam.oxxostudio.tw'
+                                )
+                            ]
                         )
                     ]
                 )

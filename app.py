fd607d385d267a37723accd5a5144603c070c2e7
chin0113 <97650035+chin0113@users.noreply.github.com>
2024-01-22 23:03:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import requests, json
 headers = {'Authorization':'1PlQGmb524SP8EccC6ZKIvX47fzf0u9pRZy0E4oCjx71d5gTBTy2U+JzlcfWMc10r4haBWSJHSv7kIE/cnRCnFM6VNtF3CMmTzVAR7n7xtlyiJs3RuuMXhPq+xOv4f9IJontF4iVL8amDiYMJlUxCAdB04t89/1O/w1cDnyilFU=','Content-Type':'application/json'}
 body = {
-    'replyToken':replyToken,
+    'replyToken':'ddd4a0b2e26f45278fdda78cd481e6d8',
     'messages':[{
             'type': 'text',
             'text': 'hello'

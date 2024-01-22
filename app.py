4f3cf5d4d410081b544471c1d4a36c39fc69157e
chin0113 <97650035+chin0113@users.noreply.github.com>
2024-01-22 13:24:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -63,11 +63,11 @@ def handle_sticker_message(event):
     try:
         body = request.get_data(as_text=True)
         json_data = json.loads(body)
-        print(json_data)
-        stickerId = events.message.stickerId
-        packageId = events.message.packageId
-        # stickerId = json_data['events'][0]['message']['stickerId'] # 取得 stickerId
-        # packageId = json_data['events'][0]['message']['packageId'] # 取得 packageId
+        # print(json_data)
+        # stickerId = events.message.stickerId
+        # packageId = events.message.packageId
+        stickerId = json_data['events'][0]['message']['stickerId'] # 取得 stickerId
+        packageId = json_data['events'][0]['message']['packageId'] # 取得 packageId
         sticker_message = StickerSendMessage(sticker_id=stickerId, package_id=packageId) # 設定要回傳的表情貼圖
         line_bot_api.reply_message(event.reply_token, sticker_message)
     except:

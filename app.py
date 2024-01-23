11151d63cedfe7dc758151277409c343645ccc35
chin0113 <97650035+chin0113@users.noreply.github.com>
2024-01-23 23:54:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import json
 import os
 import requests
+from io import BytesIO
 from flask import Flask, request, abort
 from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
@@ -13,6 +14,10 @@ app = Flask(__name__)
 def linebot():
     body = request.get_data(as_text=True)
     json_data = json.loads(body)
+    
+    image_url = 'https://steam.oxxostudio.tw/download/python/line-rich-menu-demo.jpg'
+    image_response = requests.get(image_url)
+
     # print(json_data)
 
     try:
@@ -27,8 +32,9 @@ def linebot():
         tk = json_data["events"][0]["replyToken"]  # 取得 reply token
 
         if tp == "text":
-            with open('https://steam.oxxostudio.tw/download/python/line-rich-menu-demo.jpg', 'rb') as f:
-                line_bot_api.set_rich_menu_image('richmenu-b04102d54c3ba22bada7e26fe26215b1', 'image/jpeg', f)
+            if image_response.status_code == 200:
+                with BytesIO(image_response.content) as image_buffer:
+                    line_bot_api.set_rich_menu_image('richmenu-b04102d54c3ba22bada7e26fe26215b1', 'image/jpeg', image_buffer)
             headers = {'Authorization':'Bearer 1PlQGmb524SP8EccC6ZKIvX47fzf0u9pRZy0E4oCjx71d5gTBTy2U+JzlcfWMc10r4haBWSJHSv7kIE/cnRCnFM6VNtF3CMmTzVAR7n7xtlyiJs3RuuMXhPq+xOv4f9IJontF4iVL8amDiYMJlUxCAdB04t89/1O/w1cDnyilFU='}
             req = requests.request('POST', 'https://api.line.me/v2/bot/user/all/richmenu/richmenu-b04102d54c3ba22bada7e26fe26215b1', headers=headers)
 

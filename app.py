84af7b34c1d29bd6be7885440c72dcb743882050
chin0113 <97650035+chin0113@users.noreply.github.com>
2024-01-23 22:46:04 +0800

    Update app.py

--- app.py
+++ app.py
@@ -28,20 +28,157 @@ def linebot():
         if tp == "text":
             line_bot_api.push_message(
                 "U2574668b48e37ef5423509b4e2355321",
-                TemplateSendMessage(
-                    alt_text="ImageCarousel template",
-                    template=ImageCarouselTemplate(
-                        columns=[
-                            ImageCarouselColumn(
-                                image_url="https://upload.wikimedia.org/wikipedia/en/a/a6/Pok%C3%A9mon_Pikachu_art.png",
-                                action=MessageAction(label="皮卡丘", text="皮卡丘"),
-                            ),
-                            ImageCarouselColumn(
-                                image_url="https://upload.wikimedia.org/wikipedia/en/5/59/Pok%C3%A9mon_Squirtle_art.png",
-                                action=MessageAction(label="傑尼龜", text="傑尼龜"),
-                            ),
-                        ]
-                    ),
+                FlexSendMessage(
+                    alt_text="hello",
+                    contents={
+                        "type": "bubble",
+                        "hero": {
+                            "type": "image",
+                            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png",
+                            "size": "full",
+                            "aspectRatio": "20:13",
+                            "aspectMode": "cover",
+                            "action": {"type": "uri", "uri": "http://linecorp.com/"},
+                        },
+                        "body": {
+                            "type": "box",
+                            "layout": "vertical",
+                            "contents": [
+                                {
+                                    "type": "text",
+                                    "text": "Brown Cafe",
+                                    "weight": "bold",
+                                    "size": "xl",
+                                },
+                                {
+                                    "type": "box",
+                                    "layout": "baseline",
+                                    "margin": "md",
+                                    "contents": [
+                                        {
+                                            "type": "icon",
+                                            "size": "sm",
+                                            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
+                                        },
+                                        {
+                                            "type": "icon",
+                                            "size": "sm",
+                                            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
+                                        },
+                                        {
+                                            "type": "icon",
+                                            "size": "sm",
+                                            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
+                                        },
+                                        {
+                                            "type": "icon",
+                                            "size": "sm",
+                                            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gold_star_28.png",
+                                        },
+                                        {
+                                            "type": "icon",
+                                            "size": "sm",
+                                            "url": "https://scdn.line-apps.com/n/channel_devcenter/img/fx/review_gray_star_28.png",
+                                        },
+                                        {
+                                            "type": "text",
+                                            "text": "4.0",
+                                            "size": "sm",
+                                            "color": "#999999",
+                                            "margin": "md",
+                                            "flex": 0,
+                                        },
+                                    ],
+                                },
+                                {
+                                    "type": "box",
+                                    "layout": "vertical",
+                                    "margin": "lg",
+                                    "spacing": "sm",
+                                    "contents": [
+                                        {
+                                            "type": "box",
+                                            "layout": "baseline",
+                                            "spacing": "sm",
+                                            "contents": [
+                                                {
+                                                    "type": "text",
+                                                    "text": "Place",
+                                                    "color": "#aaaaaa",
+                                                    "size": "sm",
+                                                    "flex": 1,
+                                                },
+                                                {
+                                                    "type": "text",
+                                                    "text": "Miraina Tower, 4-1-6 Shinjuku, Tokyo",
+                                                    "wrap": True,
+                                                    "color": "#666666",
+                                                    "size": "sm",
+                                                    "flex": 5,
+                                                },
+                                            ],
+                                        },
+                                        {
+                                            "type": "box",
+                                            "layout": "baseline",
+                                            "spacing": "sm",
+                                            "contents": [
+                                                {
+                                                    "type": "text",
+                                                    "text": "Time",
+                                                    "color": "#aaaaaa",
+                                                    "size": "sm",
+                                                    "flex": 1,
+                                                },
+                                                {
+                                                    "type": "text",
+                                                    "text": "10:00 - 23:00",
+                                                    "wrap": True,
+                                                    "color": "#666666",
+                                                    "size": "sm",
+                                                    "flex": 5,
+                                                },
+                                            ],
+                                        },
+                                    ],
+                                },
+                            ],
+                        },
+                        "footer": {
+                            "type": "box",
+                            "layout": "vertical",
+                            "spacing": "sm",
+                            "contents": [
+                                {
+                                    "type": "button",
+                                    "style": "link",
+                                    "height": "sm",
+                                    "action": {
+                                        "type": "uri",
+                                        "label": "CALL",
+                                        "uri": "https://linecorp.com",
+                                    },
+                                },
+                                {
+                                    "type": "button",
+                                    "style": "link",
+                                    "height": "sm",
+                                    "action": {
+                                        "type": "uri",
+                                        "label": "WEBSITE",
+                                        "uri": "https://linecorp.com",
+                                    },
+                                },
+                                {
+                                    "type": "box",
+                                    "layout": "vertical",
+                                    "contents": [],
+                                    "margin": "sm",
+                                },
+                            ],
+                            "flex": 0,
+                        },
+                    },
                 ),
             )
 

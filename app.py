d8595bd9e3e9ede08a38c314fc99d75a3044ac2b
chin0113 <97650035+chin0113@users.noreply.github.com>
2024-01-27 01:42:34 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import json, os, requests, time, pytz
+import json, os, requests, time, pytz, statistics
 from datetime import datetime
 from io import BytesIO
 from flask import Flask, request, abort
@@ -56,6 +56,69 @@ def push_message(msg, uid, token):
         data=json.dumps(body).encode("utf-8")
     )
 
+def current_weather(address):
+    city_list, area_list, area_list2 = {}, {}, {} # 定義好待會要用的變數
+    msg = '找不到氣象資訊。'                         # 預設回傳訊息
+    
+     # 定義取得資料的函式
+    def get_data(url):
+        w_data = requests.get(url)   # 爬取目前天氣網址的資料
+        w_data_json = w_data.json()  # json 格式化訊息內容
+        location = w_data_json['cwaopendata']['dataset']['Station']  # 取出對應地點的內容
+        
+        for i in location:
+            # name = i['Station']['StationName']               # 測站地點
+            city = i['GeoInfo']['CountyName']     # 縣市名稱
+            area = i['GeoInfo']['TownName']       # 鄉鎮行政區
+            
+            temp = check_data(i['WeatherElement']['AirTemperature'])        # 氣溫
+            humd = check_data(i['WeatherElement']['RelativeHumidity'])      # 相對濕度
+            r24 = check_data(i['WeatherElement']['Now']['Precipitation'])   # 累積雨量
+            
+            if area not in area_list:
+                area_list[area] = {'temp':temp, 'humd':humd, 'r24':r24}  # 以鄉鎮區域為 key，儲存需要的資訊
+                
+            if city not in city_list:
+                city_list[city] = {'temp':[], 'humd':[], 'r24':[]}       # 以主要縣市名稱為 key，準備紀錄裡面所有鄉鎮的數值
+            
+            city_list[city]['temp'].append(temp)   # 記錄主要縣市裡鄉鎮區域的溫度 ( 串列格式 )
+            city_list[city]['humd'].append(humd)   # 記錄主要縣市裡鄉鎮區域的濕度 ( 串列格式 )
+            city_list[city]['r24'].append(r24)     # 記錄主要縣市裡鄉鎮區域的雨量 ( 串列格式 )
+    
+    # 定義如果數值小於 0，回傳 False 的函式
+    def check_data(e):
+        return False if float(e)<0 else float(e)
+        
+    def msg_content(loc, msg):
+        a = msg
+        for i in loc:
+            if i in address: # 如果地址裡存在 key 的名稱
+                temp = f"氣溫 {loc[i]['temp']} 度，" if loc[i]['temp'] != False else ''
+                humd = f"相對濕度 {loc[i]['humd']}%，" if loc[i]['humd'] != False else ''
+                r24 = f"累積雨量 {loc[i]['r24']}mm" if loc[i]['r24'] != False else ''
+                description = f'{temp}{humd}{r24}'.strip('，')
+                a = f'{description}。' # 取出 key 的內容作為回傳訊息使用
+                break
+        return a
+    
+    try:
+        # 因為目前天氣有兩組網址，兩組都爬取
+        code = 'CWA-DAEAB112-B74E-41D8-B951-527F63665E26'
+        get_data(f'https://opendata.cwa.gov.tw/fileapi/v1/opendataapi/O-A0001-001?Authorization={code}&downloadType=WEB&format=JSON')
+        get_data(f'https://opendata.cwa.gov.tw/fileapi/v1/opendataapi/O-A0003-001?Authorization={code}&downloadType=WEB&format=JSON')
+
+        for i in city_list:
+            if i not in area_list2: # 將主要縣市裡的數值平均後，以主要縣市名稱為 key，再度儲存一次，如果找不到鄉鎮區域，就使用平均數值
+                area_list2[i] = {'temp':round(statistics.mean(city_list[i]['temp']),1),
+                                'humd':round(statistics.mean(city_list[i]['humd']),1),
+                                'r24':round(statistics.mean(city_list[i]['r24']),1)
+                                }
+        msg = msg_content(area_list2, msg)  # 將訊息改為「大縣市」
+        msg = msg_content(area_list, msg)   # 將訊息改為「鄉鎮區域」
+        return msg    # 回傳 msg
+    except:
+        return msg    # 如果取資料有發生錯誤，直接回傳 msg
+        
 @app.route("/callback", methods=["POST"])
 def linebot():
 
@@ -128,6 +191,7 @@ def linebot():
                 
         if tp == "location":
             address = json_data['events'][0]['message']['address'].replace('台','臺')   # 取出地址資訊，並將「台」換成「臺」
+            reply_message(f'{address}\n\n{current_weather(address)}', tk, os.getenv("CHANNEL_ACCESS_TOKEN"))
             print(address)
             
 
@@ -138,6 +202,3 @@ def linebot():
 
 if __name__ == "__main__":
     app.run()
-
-
-

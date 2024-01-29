a23f899d978c99954b634dd6085bf4a5533b9f27
chin0113 <97650035+chin0113@users.noreply.github.com>
2024-01-29 19:00:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -279,14 +279,14 @@ def linebot():
             with BytesIO(image_response.content) as image_buffer:
                 line_bot_api.set_rich_menu_image('richmenu-4754c2730b2fed025a9c6e91d555f5b5', 'image/jpeg', image_buffer)
                 # rich_menu(richmenu_id, os.getenv("CHANNEL_SECRET"))
-        line_bot_api.set_rich_menu_image('richmenu-4754c2730b2fed025a9c6e91d555f5b5', 'image/jpeg', image_buffer)
-        headers = {"Authorization": "Bearer 1PlQGmb524SP8EccC6ZKIvX47fzf0u9pRZy0E4oCjx71d5gTBTy2U+JzlcfWMc10r4haBWSJHSv7kIE/cnRCnFM6VNtF3CMmTzVAR7n7xtlyiJs3RuuMXhPq+xOv4f9IJontF4iVL8amDiYMJlUxCAdB04t89/1O/w1cDnyilFU="}
-        req = requests.request(
-            'POST', 
-            'https://api.line.me/v2/bot/user/all/richmenu/richmenu-4754c2730b2fed025a9c6e91d555f5b5', 
-            headers=headers
-        )                   
-        print(req.text)
+                line_bot_api.set_rich_menu_image('richmenu-4754c2730b2fed025a9c6e91d555f5b5', 'image/jpeg', image_buffer)
+                headers = {"Authorization": "Bearer 1PlQGmb524SP8EccC6ZKIvX47fzf0u9pRZy0E4oCjx71d5gTBTy2U+JzlcfWMc10r4haBWSJHSv7kIE/cnRCnFM6VNtF3CMmTzVAR7n7xtlyiJs3RuuMXhPq+xOv4f9IJontF4iVL8amDiYMJlUxCAdB04t89/1O/w1cDnyilFU="}
+                req = requests.request(
+                    'POST', 
+                    'https://api.line.me/v2/bot/user/all/richmenu/richmenu-4754c2730b2fed025a9c6e91d555f5b5', 
+                    headers=headers
+                )                   
+                print(req.text)
         
         tp = json_data["events"][0]["message"]["type"]
         tk = json_data["events"][0]["replyToken"]  # 取得 reply token

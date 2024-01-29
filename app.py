3b0e3aab4e986ddf06c97e7bbe975aad75bd86e7
chin0113 <97650035+chin0113@users.noreply.github.com>
2024-01-29 19:22:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -260,7 +260,10 @@ def aqi(address):
 def linebot():
     body = request.get_data(as_text=True)
     json_data = json.loads(body)
-
+    
+    image_url = 'https://steam.oxxostudio.tw/download/python/line-rich-menu-switch-demo-a.jpg'
+    image_response = requests.get(image_url)
+    
     print(json_data)
 
     try:
@@ -271,22 +274,17 @@ def linebot():
         signature = request.headers["X-Line-Signature"]
         handler.handle(body, signature)
         
-        # image_url = 'https://steam.oxxostudio.tw/download/python/line-bot-weather-demo.jpg'
-        # richmenu_id = 'richmenu-a69b8e585f6d72952a989ff08e824d53'
-        
-        image_response = requests.get('https://steam.oxxostudio.tw/download/python/line-rich-menu-demo.jpg')
         if image_response.status_code == 200:
-            image_buffer = BytesIO(image_response.content)
-            #with BytesIO(image_response.content) as image_buffer:
-            line_bot_api.set_rich_menu_image('richmenu-4754c2730b2fed025a9c6e91d555f5b5', 'image/jpeg', image_buffer)
-            # rich_menu(richmenu_id, os.getenv("CHANNEL_SECRET"))
-            headers = {"Authorization": "Bearer 1PlQGmb524SP8EccC6ZKIvX47fzf0u9pRZy0E4oCjx71d5gTBTy2U+JzlcfWMc10r4haBWSJHSv7kIE/cnRCnFM6VNtF3CMmTzVAR7n7xtlyiJs3RuuMXhPq+xOv4f9IJontF4iVL8amDiYMJlUxCAdB04t89/1O/w1cDnyilFU=", "Content-Type":"application/json"}
-            req = requests.request(
-                'POST', 
-                'https://api.line.me/v2/bot/user/all/richmenu/richmenu-4754c2730b2fed025a9c6e91d555f5b5', 
-                headers=headers
-            )                   
-            print(req.text)
+            with BytesIO(image_response.content) as image_buffer:
+                line_bot_api.set_rich_menu_image('richmenu-bbe5902cc4e8d577e8c0f55a8d3af91b', 'image/jpeg', image_buffer)
+        
+        headers = {"Authorization": "Bearer 1PlQGmb524SP8EccC6ZKIvX47fzf0u9pRZy0E4oCjx71d5gTBTy2U+JzlcfWMc10r4haBWSJHSv7kIE/cnRCnFM6VNtF3CMmTzVAR7n7xtlyiJs3RuuMXhPq+xOv4f9IJontF4iVL8amDiYMJlUxCAdB04t89/1O/w1cDnyilFU=", "Content-Type":"application/json"}
+        req = requests.request(
+            'POST', 
+            'https://api.line.me/v2/bot/user/all/richmenu/richmenu-bbe5902cc4e8d577e8c0f55a8d3af91b', 
+            headers=headers
+        )                   
+        print(req.text)
             
         tp = json_data["events"][0]["message"]["type"]
         tk = json_data["events"][0]["replyToken"]  # 取得 reply token
